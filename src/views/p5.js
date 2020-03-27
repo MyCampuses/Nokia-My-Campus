61f46610253276bbd7bbd98858a6cb3da7161e65
Baron <jukki.grotenfelt@gmail.com>
2020-03-27 17:48:23 +0200

    test

--- src/views/p5.js
+++ src/views/p5.js
@@ -1,5 +1,24 @@
 /* eslint-disable no-unused-vars */
+import React, {useState} from 'react';
+import '../styles/App.css';
+import {
+    Container
+} from '@material-ui/core'
+
+import '../styles/p5.css';
+import strings from "../localization";
+import API from "../hooks/ApiHooks";
+import LocalStorageOperations from "../hooks/LocalStorageOperations";
+
+
 const P5 = (props) =>{
 
+    return(
+        <Container component="mainp5" maxWidth="xs">
+            <div className="p5 main">
+
+            </div>
+        </Container>
+    )
 };
 export default P5;
