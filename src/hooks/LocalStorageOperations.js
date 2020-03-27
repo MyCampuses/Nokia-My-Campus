84c4b22d552f20db906e39fc5bb1dc1c6ebffb97
unknown <arttujo@gmail.com>
2020-03-27 14:49:03 +0200

    added LocalStorage for saving users name and token

---
+++ src/hooks/LocalStorageOperations.js
@@ -0,0 +1,34 @@
+
+const LocalStorageOperations=()=> {
+
+    const myStorage = window.localStorage
+
+    const create = (data,key) =>{
+        myStorage.setItem(key,data);
+        console.log(`Item Saved: ${data}`)
+    };
+
+    const read = (key) =>{
+        let item = myStorage.getItem(key);
+        return JSON.parse(item)
+    };
+
+    const del = (key) =>{
+        myStorage.removeItem(key)
+        console.log(`Item deleted for key: ${key}`)
+    };
+
+    const clear = () =>{
+      myStorage.clear()
+        console.log("LocalStorage Cleared")
+    };
+
+    return {
+        create,
+        read,
+        del,
+        clear
+    }
+};
+
+export default LocalStorageOperations
