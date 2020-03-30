e4da016216d90b384a57149f3365d828ab029a7c
Baron <jukki.grotenfelt@gmail.com>
2020-03-30 16:30:21 +0300

    test

--- src/views/p5.js
+++ src/views/p5.js
@@ -2,7 +2,12 @@
 import React, {useState} from 'react';
 import '../styles/App.css';
 import {
-    Container
+    Container,
+    AppBar,
+    Toolbar,
+    IconButton,
+    Typography,
+
 } from '@material-ui/core'
 
 import '../styles/p5.css';
@@ -13,8 +18,19 @@ import LocalStorageOperations from "../hooks/LocalStorageOperations";
 
 const P5 = (props) =>{
 
+    const {navigation} = props
+
     return(
         <Container component="main_p5" maxWidth="xs">
+            <AppBar position="static">
+                <Toolbar variant="dense">
+                    <IconButton edge="start" className="MenuButton" color="inherit" aria-label="menu">
+                    </IconButton>
+                    <Typography variant="h6" color="inherit">
+                        Photos
+                    </Typography>
+                </Toolbar>
+            </AppBar>
             <div className="p5_main">
                 <h1>Inside levels P5</h1>
                 <h3>Live Utilization</h3>
@@ -22,4 +38,5 @@ const P5 = (props) =>{
         </Container>
     )
 };
+
 export default P5;
