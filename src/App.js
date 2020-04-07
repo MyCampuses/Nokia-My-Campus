26984aec11afbf6c6eb5d1614b0d0031fa2ff040
Janjhy <jani.hyrkas@gmail.com>
2020-04-07 13:10:08 +0300

    Changed container to div in restaurant. Added static bg color in restaurant.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 /* eslint-disable no-unused-vars */
-import React, {useEffect} from 'react';
-import {useRoutes, useRedirect,navigate} from 'hookrouter';
+import React from 'react';
+import {useRoutes} from 'hookrouter';
 import './styles/App.css';
 import routes from './hooks/Routes';
 import Authentication from './hooks/Authentication';
-import Login from "./views/login";
-import Home from './views/home';
 
 
 const App = () => {
-  const {isLoggedIn} = Authentication();
-  const routeResult = useRoutes(routes);
-  //useRedirect('/login','/home', );
+    const {isLoggedIn} = Authentication();
+    const routeResult = useRoutes(routes);
 
-  return (
-      <div className="App">
-          {routeResult}
-      </div>
-  );
+    return (
+        <div className="App">
+            {routeResult}
+        </div>
+    );
 };
 /* eslint-enable no-unused-vars */
 export default App;
