40514625c6a240d1b168103993a761fe07d57e42
magmake <markus.saronsalo@metropolia.fi>
2020-04-14 16:10:57 +0300

    AppJsremovedLines

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
-/* eslint-disable no-unused-vars */
 import React from 'react';
 import {useRoutes} from 'hookrouter';
 import './styles/App.css';
 import routes from './hooks/Routes';
-import Authentication from './hooks/Authentication';
-import LocalStorageOperations from './hooks/LocalStorageOperations';
 
 
 const App = () => {
     const routeResult = useRoutes(routes);
-
     return (
         <div className="App">
             {routeResult}
         </div>
     );
 };
-/* eslint-enable no-unused-vars */
 export default App;
