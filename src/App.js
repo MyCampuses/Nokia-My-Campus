ebc3ce955fcb11e62541a1eda64f891cb7d1fa6d
unknown <arttujo@gmail.com>
2020-04-14 14:01:20 +0300

    route and manifest change

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import './styles/App.css';
 import routes from './hooks/Routes';
 import Authentication from './hooks/Authentication';
 import LocalStorageOperations from './hooks/LocalStorageOperations';
+import * as serviceWorker from './serviceWorker';
 
 
 const App = () => {
     const routeResult = useRoutes(routes);
-
     return (
         <div className="App">
             {routeResult}
