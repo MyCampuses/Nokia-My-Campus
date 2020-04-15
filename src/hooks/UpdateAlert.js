5945d82c345beeae1f50b70545cc337ab3bae926
magmake <markus.saronsalo@metropolia.fi>
2020-04-15 21:42:24 +0300

    SWIteration5

--- src/hooks/UpdateAlert.js
+++ src/hooks/UpdateAlert.js
@@ -75,7 +75,7 @@ const UpdateAlert = () => {
       }
     };
     return <div className="App-alert">
-      {isServiceWorkerInitialized}
+      {isServiceWorkerInitialized && <Alert text="Service worker is initialized" type={SW_INIT}>}
       {isServiceWorkerUpdated && (
           <Alert text="New version of the app available" buttonText="Update"
                  type={SW_UPDATE} onClick={updateServiceWorker()}/>
