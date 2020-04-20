55f1ea2ec6913585e2b8a5f7557da4812b6a2317
magmake <markus.saronsalo@metropolia.fi>
2020-04-20 16:56:07 +0300

    MoreSWFix

--- src/hooks/UpdateServiceWorker.js
+++ src/hooks/UpdateServiceWorker.js
@@ -1,17 +1,19 @@
-import React, {useEffect} from 'react';
+import {useEffect} from 'react';
 import {useDispatch} from 'react-redux';
 
 // Create alert with redux
 const Update = ({type, onUpdate}) => {
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch({type});
-    }, []);// eslint-disable-line
-  return (
-      <div className="updateAlert">
-        {onUpdate}
-      </div>
-  );
+    if (!onUpdate) {
+      const timer = setTimeout(() => {
+        dispatch({type});
+      }, 3000);
+      return () => {
+        clearTimeout(timer);
+      };
+    }
+  }, []);// eslint-disable-line
 };
 
 export default Update;
