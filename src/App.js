f418b4ccc4d9487393813dd76c405b48f6f0a83b
magmake <markus.saronsalo@metropolia.fi>
2020-04-20 17:27:28 +0300

    useEffectTest

--- src/App.js
+++ src/App.js
@@ -18,7 +18,8 @@ const App = () => {
       state => state.serviceWorkerRegistration);
   useEffect(() => {
     const registrationWaiting = serviceWorkerRegistration.waiting;
-
+    console.log(registrationWaiting)
+    console.log("Checking SW state")
     if (registrationWaiting) {
       registrationWaiting.postMessage({type: 'SKIP_WAITING'});
       registrationWaiting.addEventListener('statechange', event => {
