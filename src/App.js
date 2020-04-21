10f613111e80868d6840ed917ca8d1250d608fc1
magmake <markus.saronsalo@metropolia.fi>
2020-04-21 17:17:29 +0300

    AlertTest

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {useRoutes} from 'hookrouter';
 import './styles/App.css';
 import routes from './hooks/Routes';
-import Update from './Update'
 
 const App = () => {
   const routeResult = useRoutes(routes);
