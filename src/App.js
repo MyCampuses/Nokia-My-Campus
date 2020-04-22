e58e4169f3f8fb86f288f9248ccccca56bb9beb4
magmake <markus.saronsalo@metropolia.fi>
2020-04-22 17:47:51 +0300

    SwAlertTestv501

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {useRoutes} from 'hookrouter';
 import './styles/App.css';
+import Update from './Update';
 import routes from './hooks/Routes';
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
   return (
       <div className="App">
         {routeResult}
+        <Update/>
       </div>
 
 
