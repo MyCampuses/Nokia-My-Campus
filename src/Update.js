fc1f7b5b18724f063d02b572ddc14624a32425f6
magmake <markus.saronsalo@metropolia.fi>
2020-04-28 16:18:41 +0300

    AlertTest3

--- src/Update.js
+++ src/Update.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import {useSelector} from 'react-redux';
 import Data from './hooks/Data';
-import UpdateApp from './hooks/UpdateServiceWorker'
+import UpdateApp from './hooks/UpdateServiceWorker';
+
 const {SW_INIT, SW_UPDATE} = Data();
-const {confirmAlert} = 'react-confirm-alert'
+const {confirmAlert} = 'react-confirm-alert';
 
 const Update = () => {
   // Defining constants for redux depending on serviceworker state
@@ -20,37 +21,44 @@ const Update = () => {
     if (registrationWaiting) {
       registrationWaiting.postMessage({type: 'SKIP_WAITING'});
       registrationWaiting.addEventListener('statechange', () => {
-        confirmAlert({
-          title: 'App Update',
-          message: 'App has been Updated, refreshing app',
-          buttons: [
-            {
-              label: 'Ok',
-              onClick: () => window.location.reload()
-            }
-          ],
-          onClick: window.location.reload()
-        })
-      });
-    }
-  }
-  return (
-      // UpdateApp in UpdateServiceWorker.js, SW_INIT and SW_UPDATE is used with redux dispatch depending on serviceworker state
-      <div>
+        const addAlert = ({onClose}) => {
+          const handleClick = () => {
+            const timer = setTimeout(() => {
+              window.location.reload();
+              onClose();
+            }, 2000);
+            // Clear timeout so timer works correctly every time
+            return () => clearTimeout(timer);
+          };
+          return (
+              <div className="AppUpdated">
+                <h1>App Updated</h1>
+                <p>App has been Updated, refreshing app</p>
+                <div className="DialogButton">
+                  <button onClick={handleClick}>Ok</button>
+                </div>
+            </div>
+          )
+        }
+        confirmAlert({ customUI: addAlert})
+
+        return (
+        // UpdateApp in UpdateServiceWorker.js, SW_INIT and SW_UPDATE is used with redux dispatch depending on serviceworker state
+        <div>
         {isServiceWorkerInitialized && (
-            <UpdateApp
-                type={SW_INIT}
-            />
-          )}
-          {isServiceWorkerUpdated && (
-            <UpdateApp
-                type={SW_UPDATE}
-                onUpdate={updateServiceWorker()}
-            />
+        <UpdateApp
+        type={SW_INIT}
+        />
+        )}
+        {isServiceWorkerUpdated && (
+        <UpdateApp
+        type={SW_UPDATE}
+        onUpdate={updateServiceWorker()}
+        />
         )}
 
-      </div>
+        </div>
 
-  );
-};
-export default Update;
+        );
+        };
+        export default Update;
