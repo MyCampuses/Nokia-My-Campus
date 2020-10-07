7cef164e2c480ea4b5997fc45695b5077511f0e3
Baron <jukki.grotenfelt@gmail.com>
2020-10-07 14:36:30 +0300

    Updates

---
+++ precache-manifest.e944b042b32b53c09ecdfe0853a1fed9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aff970cc9bddad92f9c4c39742b081b0",
+    "url": "/Nokia-My-Campus/index.html"
+  },
+  {
+    "revision": "543f11a4884170bab4a8",
+    "url": "/Nokia-My-Campus/static/css/main.1087c0aa.chunk.css"
+  },
+  {
+    "revision": "03e5a82878a9ac9f4cb9",
+    "url": "/Nokia-My-Campus/static/js/2.0c008f9e.chunk.js"
+  },
+  {
+    "revision": "1dbbf49ed49c0f8322d38f5475b4e94e",
+    "url": "/Nokia-My-Campus/static/js/2.0c008f9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "543f11a4884170bab4a8",
+    "url": "/Nokia-My-Campus/static/js/main.1815dc42.chunk.js"
+  },
+  {
+    "revision": "bffb2224039540f6ddb6",
+    "url": "/Nokia-My-Campus/static/js/runtime-main.d6b26c2c.js"
+  },
+  {
+    "revision": "4a2c04491a574e4fa6e07674e6f1b515",
+    "url": "/Nokia-My-Campus/static/media/campus_map_P10.4a2c0449.webp"
+  },
+  {
+    "revision": "dc26fde904b4a821ae9554ac07497343",
+    "url": "/Nokia-My-Campus/static/media/campus_map_P5.dc26fde9.webp"
+  },
+  {
+    "revision": "019333e4ced18fa7eff75fb0a5a7f217",
+    "url": "/Nokia-My-Campus/static/media/loginBackground.019333e4.png"
+  },
+  {
+    "revision": "e8cf586fe2fc978cbf4bb78e15916366",
+    "url": "/Nokia-My-Campus/static/media/logo_mycampus.e8cf586f.webp"
+  }
+]);
