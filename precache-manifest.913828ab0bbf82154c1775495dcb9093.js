d2ba511fe5bbbf1d9a110aa2a609e4a931b2c50a
Baron <jukki.grotenfelt@gmail.com>
2020-10-13 10:54:34 +0300

    Updates

---
+++ precache-manifest.913828ab0bbf82154c1775495dcb9093.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f07c446e6c110a7bfe049c722618182",
+    "url": "/Nokia-My-Campus/index.html"
+  },
+  {
+    "revision": "c17bc04a271380b08871",
+    "url": "/Nokia-My-Campus/static/css/main.1087c0aa.chunk.css"
+  },
+  {
+    "revision": "c1117ccbd9f8f4cc02d6",
+    "url": "/Nokia-My-Campus/static/js/2.647e1a09.chunk.js"
+  },
+  {
+    "revision": "1dbbf49ed49c0f8322d38f5475b4e94e",
+    "url": "/Nokia-My-Campus/static/js/2.647e1a09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c17bc04a271380b08871",
+    "url": "/Nokia-My-Campus/static/js/main.8f49b809.chunk.js"
+  },
+  {
+    "revision": "bffb2224039540f6ddb6",
+    "url": "/Nokia-My-Campus/static/js/runtime-main.d6b26c2c.js"
+  },
+  {
+    "revision": "4a2c04491a574e4fa6e07674e6f1b515",
+    "url": "/Nokia-My-Campus/static/media/campus_map_P10.4a2c0449.webp"
+  },
+  {
+    "revision": "dc26fde904b4a821ae9554ac07497343",
+    "url": "/Nokia-My-Campus/static/media/campus_map_P5.dc26fde9.webp"
+  },
+  {
+    "revision": "019333e4ced18fa7eff75fb0a5a7f217",
+    "url": "/Nokia-My-Campus/static/media/loginBackground.019333e4.png"
+  },
+  {
+    "revision": "e8cf586fe2fc978cbf4bb78e15916366",
+    "url": "/Nokia-My-Campus/static/media/logo_mycampus.e8cf586f.webp"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.6cfc335aac2658cf95962c0aabb03028.js"
+  "/Nokia-My-Campus/precache-manifest.913828ab0bbf82154c1775495dcb9093.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Nokia-My-Campus/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
