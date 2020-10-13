a9225cd20c6592e897426bd9c95cdd89b9393ba5
Baron <jukki.grotenfelt@gmail.com>
2020-10-13 10:52:55 +0300

    Updates

---
+++ precache-manifest.6cfc335aac2658cf95962c0aabb03028.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6e17d65a1ac6ca160a284a81f1388fa",
+    "url": "./index.html"
+  },
+  {
+    "revision": "da3c49424cf36bc1b3a7",
+    "url": "./static/css/main.be6fad5d.chunk.css"
+  },
+  {
+    "revision": "c1117ccbd9f8f4cc02d6",
+    "url": "./static/js/2.647e1a09.chunk.js"
+  },
+  {
+    "revision": "1dbbf49ed49c0f8322d38f5475b4e94e",
+    "url": "./static/js/2.647e1a09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da3c49424cf36bc1b3a7",
+    "url": "./static/js/main.d7ec758c.chunk.js"
+  },
+  {
+    "revision": "885e6a2f8b1e742a18b4",
+    "url": "./static/js/runtime-main.423e33cc.js"
+  },
+  {
+    "revision": "4a2c04491a574e4fa6e07674e6f1b515",
+    "url": "./static/media/campus_map_P10.4a2c0449.webp"
+  },
+  {
+    "revision": "dc26fde904b4a821ae9554ac07497343",
+    "url": "./static/media/campus_map_P5.dc26fde9.webp"
+  },
+  {
+    "revision": "019333e4ced18fa7eff75fb0a5a7f217",
+    "url": "./static/media/loginBackground.019333e4.png"
+  },
+  {
+    "revision": "e8cf586fe2fc978cbf4bb78e15916366",
+    "url": "./static/media/logo_mycampus.e8cf586f.webp"
+  }
+]);
