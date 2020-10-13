1bd10bf0714a24151650f2bf845a7dabdb43e68c
Baron <jukki.grotenfelt@gmail.com>
2020-10-13 13:25:50 +0300

    Updates

---
+++ precache-manifest.4316399d07a43e9927ceea6d4856dc49.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "789c3448ee48aba1dc3e8090a627abc3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec5f9f665a9994cd12a0",
+    "url": "/static/css/main.6e0eecec.chunk.css"
+  },
+  {
+    "revision": "c1117ccbd9f8f4cc02d6",
+    "url": "/static/js/2.647e1a09.chunk.js"
+  },
+  {
+    "revision": "1dbbf49ed49c0f8322d38f5475b4e94e",
+    "url": "/static/js/2.647e1a09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec5f9f665a9994cd12a0",
+    "url": "/static/js/main.69effc60.chunk.js"
+  },
+  {
+    "revision": "ef7985899ca638a2d087",
+    "url": "/static/js/runtime-main.ece46e95.js"
+  },
+  {
+    "revision": "4a2c04491a574e4fa6e07674e6f1b515",
+    "url": "/static/media/campus_map_P10.4a2c0449.webp"
+  },
+  {
+    "revision": "dc26fde904b4a821ae9554ac07497343",
+    "url": "/static/media/campus_map_P5.dc26fde9.webp"
+  },
+  {
+    "revision": "019333e4ced18fa7eff75fb0a5a7f217",
+    "url": "/static/media/loginBackground.019333e4.png"
+  },
+  {
+    "revision": "e8cf586fe2fc978cbf4bb78e15916366",
+    "url": "/static/media/logo_mycampus.e8cf586f.webp"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Nokia-My-Campus/precache-manifest.2a0db9f6a687bbe369d65be0d3244fee.js"
+  "/precache-manifest.4316399d07a43e9927ceea6d4856dc49.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Nokia-My-Campus/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
