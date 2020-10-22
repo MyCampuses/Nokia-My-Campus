dccd9e5b129be40c53797697009bfb9c7922f93e
Baron <jukki.grotenfelt@gmail.com>
2020-10-22 13:17:21 +0300

    Restaurant tests

--- src/tests/randomtest.test.js
+++ src/tests/randomtest.test.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom';
-import p5 from '../views/p5';
-import { act } from 'react-dom/test-utils';
+import Restaurant from '../views/restaurant';
+import NaviBar from "../fragments/TopNavigationBarFragment";
+import BottomBarTabFragment from "../fragments/BottomBarFragment";
+
 
 let container;
 
@@ -11,10 +13,35 @@ beforeEach(() => {
   });
   
   afterEach(() => {
+    ReactDOM.unmountComponentAtNode(container);
     document.body.removeChild(container);
     container = null;
   });
 
-it('Render h1 to container', () => {
-    ReactDOM.render(<h1 />, container);
+
+//This test should ALWAYS go through, it's proof that testing works.
+test('Test rendering a h1 to a empty div', () => {
+  ReactDOM.render(<h1 />, container);
+});
+
+test('Rendering restaurant top navigation bar', () => {
+  ReactDOM.render(<Restaurant />, container);
+  console.log("Checking if NaviBar Exists in restaurant");
+  const nbtrue = expect.objectContaining(NaviBar);
+  if(nbtrue){
+    console.log("Navibar exists");
+  } else {
+    console.log("Navibar does not exist");
+  };
+});
+
+test('Restaurant bottom navigation bar', () => {
+  ReactDOM.render(<Restaurant />, container);
+  console.log("Checking if bottombar Exists in restaurant");
+  const bbtrue = expect.objectContaining(BottomBarTabFragment);
+  if(bbtrue){
+    console.log("bottombar exists");
+  } else {
+    console.log("bottombar does not exist");
+  };
 });
