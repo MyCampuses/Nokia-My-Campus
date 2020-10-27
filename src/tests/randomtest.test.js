ff4a9874e45a136b6ea06d710975776066872cfa
Baron <jukki.grotenfelt@gmail.com>
2020-10-27 12:21:27 +0200

    last update for login tests

--- src/tests/randomtest.test.js
+++ src/tests/randomtest.test.js
@@ -4,57 +4,45 @@ import Restaurant from '../views/restaurant';
 import NaviBar from "../fragments/TopNavigationBarFragment";
 import BottomBarTabFragment from "../fragments/BottomBarFragment";
 import Home from '../views/home';
-import Login from '../views/login';
-import { shallow, configure } from 'enzyme';
+import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import API from '../hooks/ApiHooks';
 
-
-let container;
-let userToken;
-let login;
-const {loginAsync} = API();
 
 configure({ adapter: new Adapter() });
-
+let container;
 
 const email = 'team4.metropolia@nokia.com';
 const pw = '!t4Mycampus2020';
+
+beforeEach(() => {
+  container = document.createElement('div');
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  ReactDOM.unmountComponentAtNode(container);
+  document.body.removeChild(container);
+  container = null;
+});
 
 //This test should ALWAYS go through, it's proof that testing works.
 test('Test rendering a h1 to a empty div', () => {
   ReactDOM.render(<h1 />, container);
 });
 
+// These tests have 20h with no avail, add hours if you try this test, lets see how far it gets.
 describe(('Login tests'), () => {
-  beforeEach(() => {
-    container = mount(<Login />);
-    document.body.appendChild(container);
-  });
   
   test('Login to myCampus', () => {
-  
+    
   });
   
   test('saving user token', () => {
-  });
 
+  });
 });
 
-
-
 describe(('Restaurant Tests'), () => {
-
-  beforeEach(() => {
-    container = document.createElement('div');
-    document.body.appendChild(container);
-  });
-  
-  afterEach(() => {
-    ReactDOM.unmountComponentAtNode(container);
-    document.body.removeChild(container);
-    container = null;
-  });
 
   test('Rendering restaurant top navigation bar', () => {
     ReactDOM.render(<Restaurant />, container);
@@ -80,16 +68,6 @@ describe(('Restaurant Tests'), () => {
 });
 
 describe(('Homepage tests'), () => {
-  beforeEach(() => {
-    container = document.createElement('div');
-    document.body.appendChild(container);
-  });
-  
-  afterEach(() => {
-    ReactDOM.unmountComponentAtNode(container);
-    document.body.removeChild(container);
-    container = null;
-  });
 
   test('Navigation from homepage to all different pages', () => {
     const wrapper = (<Home />);
