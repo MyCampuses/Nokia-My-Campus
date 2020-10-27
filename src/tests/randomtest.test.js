24b5fff3761137a67f003a49c145eae5b57e369a
Baron <jukki.grotenfelt@gmail.com>
2020-10-27 11:57:20 +0200

    Added descriptions for tests

--- src/tests/randomtest.test.js
+++ src/tests/randomtest.test.js
@@ -11,8 +11,8 @@ import API from '../hooks/ApiHooks';
 
 
 let container;
-let user;
-let usertoken;
+let userToken;
+let login;
 const {loginAsync} = API();
 
 configure({ adapter: new Adapter() });
@@ -21,7 +21,31 @@ configure({ adapter: new Adapter() });
 const email = 'team4.metropolia@nokia.com';
 const pw = '!t4Mycampus2020';
 
-beforeEach(() => {
+//This test should ALWAYS go through, it's proof that testing works.
+test('Test rendering a h1 to a empty div', () => {
+  ReactDOM.render(<h1 />, container);
+});
+
+describe(('Login tests'), () => {
+  beforeEach(() => {
+    container = mount(<Login />);
+    document.body.appendChild(container);
+  });
+  
+  test('Login to myCampus', () => {
+  
+  });
+  
+  test('saving user token', () => {
+  });
+
+});
+
+
+
+describe(('Restaurant Tests'), () => {
+
+  beforeEach(() => {
     container = document.createElement('div');
     document.body.appendChild(container);
   });
@@ -32,57 +56,49 @@ beforeEach(() => {
     container = null;
   });
 
-
-test('Login to myCampus', () => {
-  const loginData = {
-    email: email,
-    password: pw,
-  };
-  const login = loginAsync(loginData);
-  if(login){
-    console.log('Login complete');
-  } else {
-    console.log('Login failed');
-  };
-  console.log(login);
+  test('Rendering restaurant top navigation bar', () => {
+    ReactDOM.render(<Restaurant />, container);
+    console.log("Checking if NaviBar Exists in restaurant");
+    const nbtrue = expect.objectContaining(NaviBar);
+    if(nbtrue){
+      console.log("Navibar exists");
+    } else {
+      console.log("Navibar does not exist");
+    };
+  });
   
+  test('Restaurant bottom navigation bar', () => {
+    ReactDOM.render(<Restaurant />, container);
+    console.log("Checking if bottombar Exists in restaurant");
+    const bbtrue = expect.objectContaining(BottomBarTabFragment);
+    if(bbtrue){
+      console.log("bottombar exists");
+    } else {
+      console.log("bottombar does not exist");
+    };
+  });
 });
 
+describe(('Homepage tests'), () => {
+  beforeEach(() => {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+  });
+  
+  afterEach(() => {
+    ReactDOM.unmountComponentAtNode(container);
+    document.body.removeChild(container);
+    container = null;
+  });
 
-//This test should ALWAYS go through, it's proof that testing works.
-test('Test rendering a h1 to a empty div', () => {
-  ReactDOM.render(<h1 />, container);
-});
-
-test('Rendering restaurant top navigation bar', () => {
-  ReactDOM.render(<Restaurant />, container);
-  console.log("Checking if NaviBar Exists in restaurant");
-  const nbtrue = expect.objectContaining(NaviBar);
-  if(nbtrue){
-    console.log("Navibar exists");
-  } else {
-    console.log("Navibar does not exist");
-  };
-});
-
-test('Restaurant bottom navigation bar', () => {
-  ReactDOM.render(<Restaurant />, container);
-  console.log("Checking if bottombar Exists in restaurant");
-  const bbtrue = expect.objectContaining(BottomBarTabFragment);
-  if(bbtrue){
-    console.log("bottombar exists");
-  } else {
-    console.log("bottombar does not exist");
-  };
-});
-
-test('Navigation from homepage to all different pages', () => {
-  const wrapper = (<Home />);
-  ReactDOM.render(wrapper, container);
-  const nbtrue = expect.objectContaining(NaviBar);
-  if(nbtrue){
-    console.log('Navibar exists');
-  } else {
-    console.log('there is no navibar');
-  };
+  test('Navigation from homepage to all different pages', () => {
+    const wrapper = (<Home />);
+    ReactDOM.render(wrapper, container);
+    const nbtrue = expect.objectContaining(NaviBar);
+    if(nbtrue){
+      console.log('Navibar exists');
+    } else {
+      console.log('there is no navibar');
+    };
+  });
 });
