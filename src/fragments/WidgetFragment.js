f8a0c848da62d6f398ad47dfca010edcffb90387
Baron <jukki.grotenfelt@gmail.com>
2020-10-28 10:18:32 +0200

    Created Widget Fragment with not functionality

---
+++ src/fragments/WidgetFragment.js
@@ -0,0 +1,17 @@
+/*
+    This function holds all the widgets for the app
+    like Homepage 
+*/
+import React, {useMemo, useState, Fragment} from 'react';
+import { Card } from '@material-ui/core';
+
+const widgets = (props) => {
+
+    const homepageWidget = () => {
+
+        return
+            <Fragment>
+
+            </Fragment>
+    };
+};
