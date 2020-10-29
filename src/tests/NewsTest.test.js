5b4d7b8942fb2d6ea7e2773a0bb79fc5c78db7fa
Rockronnie <roni.jumpponen@gmail.com>
2020-10-29 15:00:52 +0200

    Added enzyme for testing material-UI

---
+++ src/tests/NewsTest.test.js
@@ -0,0 +1,35 @@
+
+import React from 'react';
+import * as ReactDOM from 'react-dom';
+import { act } from 'react-dom/test-utils';
+import News from './../views/news';
+
+
+
+
+let container;
+
+beforeEach(() => {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+});
+
+afterEach(() => {
+    ReactDOM.unmountComponentAtNode(container);
+    document.body.removeChild(container);
+    container = null;
+});
+
+//This test should ALWAYS go through, it's proof that testing works.
+test('Test rendering a div', () => {
+    act(() => {
+        ReactDOM.render(<h1>Kek</h1>, container);
+    },);
+});
+test('Rendering News 0', () => {
+    act(() => {
+        ReactDOM.render(<News/>, container);
+    },);
+   const highlight = container.querySelector('Typography');
+   expect(highlight.textContent).toBe("Selaa Uutisia");
+});
