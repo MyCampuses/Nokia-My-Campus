115adb8a3c325fdd241b57ba2203a1f741efaf2e
Baron <jukki.grotenfelt@gmail.com>
2020-11-04 15:14:45 +0200

    Working one frontpage widget

--- src/styles/widgetStyle.js
+++ src/styles/widgetStyle.js
@@ -1,6 +1,8 @@
 import { makeStyles } from "@material-ui/core";
+import blue from '@material-ui/core/colors/blue';
 
 const WidgetStyle = () => {
+    const colorB = blue[500]
 
 const widgetStyle = makeStyles((theme) => ({
     frag: {
@@ -14,11 +16,41 @@ const widgetStyle = makeStyles((theme) => ({
     },
     logo: {
         height: 100,
-    }
+    },
+    header: {
+        fontSize: 'large',
+        textColor: 'black',
+      },
 }));
 
+const barTheme = makeStyles({
+    root: {
+      flexGrow: 1,
+    },
+    headLine: {
+      marginTop: '10px',
+      marginBottom: '10px',
+      color: colorB,
+    },
+    progressLabel: {
+      position: 'absolute',
+      zIndex: 1,
+      maxHeight: '100px',
+      height: '14vh',
+      maxWidth: '1152px',
+      width: '90%',
+    },
+    labelLocation: {
+      maxHeight: '100px',
+      height: '15vh',
+      justifyContent: 'flex-start',
+    },
+
+  });
+
 return {
-    widgetStyle: widgetStyle
+    widgetStyle: widgetStyle,
+    barTheme: barTheme,
     };
 };
 
