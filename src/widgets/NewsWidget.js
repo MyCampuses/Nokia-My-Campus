a4eb6961f93aa9fb99dab00d200f6d42058e8d1d
Rockronnie <roni.jumpponen@gmail.com>
2020-12-08 12:09:21 +0200

    Fixed news widget

--- src/widgets/NewsWidget.js
+++ src/widgets/NewsWidget.js
@@ -5,10 +5,22 @@ The function returns the element NewsBrowseGrid and the data needed for it.
 import React from 'react';
 import NewsBrowseGrid from "../fragments/NewsBrowseGrid";
 import NewsHooks from '../hooks/NewsHooks';
+import {useState, useEffect} from 'react'
 
 const NewsBrowseWidget = () =>  {
     const { getNewsItems } = NewsHooks();
-    const tileData = getNewsItems();
+    const [tileData, setTileData] = useState(null)
+
+    // fetching 10 freshed news
+  const updateNews = async () => {
+    const newsItems = await getNewsItems();
+    console.log(newsItems);
+    setTileData(newsItems);
+  };
+
+  useEffect(() => {
+    updateNews()
+  },[])//eslint-disable-line
     
     return (
         <NewsBrowseGrid tileData={tileData} />
